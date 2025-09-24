6be1e9f0c1ab1ebd532b727ac5b6ef02d7b61173
罗杰 - Roger <rmarrero@cbr11s2.42barcelona.com>
2025-09-24 21:02:21 +0200

    src/mandatory/parser/parser_utils_3.c {✓}, norma {✓}

--- src/mandatory/parser/parser_utils_3.c
+++ src/mandatory/parser/parser_utils_3.c
@@ -11,7 +11,20 @@
 /* ************************************************************************** */
 #include "../../../include/minirt.h"
 
-// Añade un objeto de forma dinámica a la escena
+/**
+ * @brief Adds a new object to the scene's list of objects.
+ *
+ * @details This function dynamically resizes the array of object pointers to
+ * accommodate a new object. It allocates a new array that is one size larger,
+ * copies all existing object pointers to the new array, frees the old array,
+ * and then adds the new object pointer to the end of the list. The array is
+ * always kept null-terminated to facilitate iteration.
+ *
+ * @param scene A pointer to the scene structure.
+ * @param obj The new object pointer to be added.
+ *
+ * @return void.
+ */
 void	add_object_to_scene(t_scene *scene, t_object *obj)
 {
 	int			count;
@@ -37,7 +50,21 @@ void	add_object_to_scene(t_scene *scene, t_object *obj)
 	scene->objects = new_objects;
 }
 
-// Añade una luz de forma dinámica a la escena
+/**
+ * @brief Adds a new light source to the scene's list of lights.
+ *
+ * @details This function dynamically resizes the array of light pointers to
+ * add a new light source. It first counts the existing lights, allocates a
+ * new array that is one size larger, copies the existing light pointers,
+ * and then frees the old array. The new light is then added, and the array
+ * is null-terminated. This function also increments the total number of
+ * lights in the scene.
+ *
+ * @param scene A pointer to the scene structure.
+ * @param light The new light pointer to be added.
+ *
+ * @return void.
+ */
 void	add_light_to_scene(t_scene *scene, t_light *light)
 {
 	int		count;
