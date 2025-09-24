d0c02e5b5b58d5274db9b46219551caa90847f50
罗杰 - Roger <rmarrero@cbr11s2.42barcelona.com>
2025-09-24 21:02:20 +0200

    src/mandatory/parser/ft_error.c {✓}, norma {✓}

--- src/mandatory/parser/ft_error.c
+++ src/mandatory/parser/ft_error.c
@@ -9,9 +9,22 @@
 /*   Updated: 2025/07/23 14:23:20 by rmarrero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "../../../include/minirt.h"
 
+/**
+ * @brief Prints an error message and exits the program with a failure status.
+ *
+
+	* @details This function is a robust error-handling routine. 
+	It writes the provided
+ * string message to the standard error stream (file descriptor 2) followed by a
+ * newline. It then immediately terminates the program, returning a non-zero
+ * status code to the operating system to indicate an unsuccessful execution.
+ *
+ * @param msg The constant character string (error message) to be displayed.
+ *
+ * @return void. The function does not return, as it terminates the program.
+ */
 void	ft_error_exit(const char *msg)
 {
 	ft_putendl_fd((char *)msg, 2);
